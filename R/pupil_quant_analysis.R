5db2843a0601e99e0908632ac0226f9883da30fb
evelyn-greeves <evgreeves@gmail.com>
2026-07-14 15:00:59 +0100

    Produce plots of pupil quant questions

--- R/pupil_quant_analysis.R
+++ R/pupil_quant_analysis.R
@@ -25,18 +25,25 @@ pupil_quant <- pupil_quant |>
 pupil_quant <- pupil_quant |>
   mutate(total_children = as.numeric(total_children),
          count = as.numeric(count)) |> 
-  mutate(count = case_when(correct == "wrong" ~ total_children - count,
-                           .default = count)) |>
+  # mutate(count = case_when(correct == "wrong" ~ total_children - count,
+  #                          .default = count)) |>
   phe_proportion(x = count,
                  n = total_children,
                  multiplier = 100)
 
-pupil_quant |> 
-  filter(!question_code %in% c("glo_gel", "colouring", "teacher_demo")) |> 
+
+# Plots -------------------------------------------------------------------
+
+# handwashing context questions
+
+p1 <- pupil_quant |> 
+  filter(!question_code %in% c("glo_gel", "colouring", "teacher_demo"),
+         correct == "right") |> 
   separate_wider_delim(full_question,
                        delim = " - ",
                        names = c("question", "context")) |> 
-  mutate(context = str_to_title(context)) |> 
+  mutate(context = str_to_title(context),
+         correct = "Correct answer = Yes") |> 
   ggplot(aes(x = value,
          y = context,
          group = phase)) +
@@ -47,7 +54,83 @@ pupil_quant |>
                     xmax = uppercl),
                 width = .2,
                 position = position_dodge(0.9)) +
-  theme_minimal() +
+  facet_wrap(~correct,
+             nrow = 2,
+             strip.position = "right") +
+  scale_fill_manual(values = c("#2c7fb8", "#a1dab4"),
+                    breaks = c("Pre-intervention", "Post-intervention")) +
+  theme_light() +
+  theme(panel.grid.major.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = margin(5.5, 5.5, 0, 5.5, "pt")) +
+  labs(y = NULL,
+       x = NULL,
+       fill = NULL)
+
+p2 <- pupil_quant |> 
+  filter(!question_code %in% c("glo_gel", "colouring", "teacher_demo"),
+         correct == "wrong") |> 
+  separate_wider_delim(full_question,
+                       delim = " - ",
+                       names = c("question", "context")) |> 
+  mutate(context = str_to_title(context),
+         correct = "Correct answer = No") |> 
+  ggplot(aes(x = value,
+             y = context,
+             group = phase)) +
+  geom_col(aes(fill = phase),
+           position = position_dodge()) +
+  geom_errorbar(aes(y = context,
+                    xmin = lowercl,
+                    xmax = uppercl),
+                width = .2,
+                position = position_dodge(0.9)) +
+  facet_wrap(~correct,
+             nrow = 2,
+             strip.position = "right") +
+  scale_fill_brewer("YlGnBu",
+                    type = "qual") +
+  theme_light() +
+  scale_fill_manual(values = c("#2c7fb8", "#a1dab4"),
+                    breaks = c("Pre-intervention", "Post-intervention")) +
+  scale_x_continuous(labels = scales::label_percent(scale = 1)) +
+  theme(panel.grid.major.y = element_blank()) +
+  labs(y = NULL,
+       fill = NULL,
+       x = "Proportion of children answered correctly")
+
+p1 / p2 +
+  plot_layout(guides = "collect",
+              axes = "collect_x") +
+  plot_annotation(title = "Do you think we should wash our hands...?") &
+  custom_theming
+
+# activity enjoyment questions
+
+pupil_quant |> 
+  filter(question_code %in% c("glo_gel", "colouring", "teacher_demo")) |> 
+  mutate(activity = str_remove(full_question,
+                                    "Did you enjoy the "),
+         activity = str_to_title(activity),
+         activity = str_replace(activity,
+                                     "Glo - Gel",
+                                     "Glo-Gel")) |> 
+  ggplot(aes(x = value,
+             y = activity)) +
+  geom_col(aes(fill = activity)) +
+  geom_errorbar(aes(y = activity,
+                    xmin = lowercl,
+                    xmax = uppercl),
+                width = .2) +
+  scale_fill_manual(values = c("#2c7fb8", "#a1dab4", "#ffffcc")) +
+  scale_x_continuous(labels = scales::label_percent(scale = 1)) +
+  theme_light() +
   custom_theming +
-  xlab("Proportion of children who answered 'Yes'") +
-  ggtitle("Do you think we should wash our hands...?")
+  theme(panel.grid.major.y = element_blank(),
+        legend.position = "none") +
+  labs(y = NULL,
+       x = "Proportion of children answered 'Yes'",
+       fill = NULL,
+       title = "Did you enjoy the...")
